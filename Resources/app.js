cbf30c2f836ce664fc2aaa9ec3d677b8ff371918
aaronksaunders <aaron@clearlyinnovative.com>
2013-05-11 00:29:31 -0400

    updated to new version of libraries

--- Resources/app.js
+++ Resources/app.js
@@ -1,65 +1,112 @@
-_ = require("underscore");
-var Backbone = require('backbone');
-Ti.include('2.5.3-crypto-sha1-hmac.js');
-require('stackmob-js-0.5.5');
-
-StackMob.init({
-	appName : 'people_interact',
-	clientSubdomain : 'clearlyinnovative',
-	publicKey : 'ADD YOUR OWN',
-	apiVersion : 0
-});
-
-Ti.API.info(' data ' + JSON.stringify(StackMob));
-
-var user = new StackMob.User({
-	username : 'toucansam',
-	password : 'fruitloops',
-});
-user.login(false, {
-	success : function(fullUserModel) {
-		console.debug('User is logged in!');
-		//The full user object is returned
-		//Ti.API.info(fullUserModel.toJSON());
-
-		var smQuery = new StackMob.Collection.Query();
-		//get all Todo items of high priority, order by createddate in ascending order.
-		smQuery.setRange(0, 4).orderAsc('username');
-		//or add them individually
-
-		var users = new StackMob.Users();
-		//use StackMob.Collection's "query" method to get your results from StackMob
-		users.query(smQuery, {
-			success : function(collection) {
-				//print out the users after the query returns from StackMob
-				console.debug(users.toJSON());
-			}
-		});
-
-	},
-	error : function(model, error) {
-		Ti.API.info(error);
-	}
-});
-
-if (false) {
-	var user = new StackMob.User({
-		username : 'toucansam',
-		password : 'fruitloops',
-		age : 15,
-		favoriteflavors : ["lemon", "blueberry", "prime rib"]
-	});
-	user.create({
-		success : function(model) {
-			Ti.API.info(model)
+ 
+// Stuff needed to make API play nice with Appcelerator
+window = {
+  location : {
+	hostname : ""
+  }
+};
+ 
+// global libraries that are expected to exists
+_ = require('underscore');
+Backbone = require('backbone');
+ 
+// this is where we just extend the StackMob module to meet the needs of 
+// Appcelerator
+var TiStackMob = function(options) {
+ 
+	// need to convert this to requires
+	Ti.include("stackmob.js");
+	var moment = require('moment');
+ 
+	StackMob.Storage = {
+ 
+		// Since the underlying client side storage implementation may not be
+		// name-spaced, we'll prefix our saved keys with `STORAGE_PREFIX`.
+		STORAGE_PREFIX : 'stackmob.',
+ 
+		//Use this to save things to local storage as a key/value pair.
+		persist : function(key, value) {
+			// If there's an HTML5 implementation of Local Storage available, then use it.
+			// Otherwise, there's no fallback at this point in time.
+			Ti.App.Properties.setString(this.STORAGE_PREFIX + key, value);
 		},
-		error : function(model, response) {
-			Ti.API.info(model);
-			Ti.API.info(response)
+		//Read a value from local storage given the `key`.
+		retrieve : function(key) {
+			return Ti.App.Properties.getString(this.STORAGE_PREFIX + key);
+		},
+		//Remove a value from local storage given the `key`.
+		remove : function(key) {
+			Ti.App.Properties.removeProperty(this.STORAGE_PREFIX + key);
 		}
-	});
-
-	Ti.API.info(StackMob.isOAuth2Mode());
-}
-
-//Ti.API.info(' data ' + JSON.stringify(user));
+	};
+ 
+	StackMob['ajax'] = function(model, params, method, options) {
+		params['beforeSend'] = function(xhr, settings) {
+			xhr.setRequestHeader("Accept", settings['accepts']);
+			if (!_.isEmpty(settings['headers'])) {
+ 
+				for (key in settings['headers']) {
+					xhr.setRequestHeader(key, settings['headers'][key]);
+				}
+			}
+		};
+ 
+		var error = params['error'];
+		params['error'] = function(jqXHR, textStatus, errorThrown) {
+			// Workaround for Android broswers not recognizing HTTP status code 206.
+			// Call the success method on HTTP Status 0 (the bug) and when a range was specified.
+			if (jqXHR.status == 0 && params['query'] && ( typeof params['query']['range'] === 'object')) {
+				this.success(jqXHR, textStatus, errorThrown);
+				return;
+			}
+			var responseText = jqXHR.responseText || jqXHR.text;
+			StackMob.onerror(jqXHR, responseText, null, model, params, error, options);
+		}
+		// Set up success callback
+		var success = params['success'];
+		var defaultSuccess = function(response, status, xhr) {
+			var result;
+ 
+			if (params["stackmob_count"] === true) {
+				result = xhr;
+			} else if (response && response.toJSON) {
+				result = response;
+			} else if (response && (response.responseText || response.text)) {
+				var json = JSON.parse(response.responseText || response.text);
+				result = json;
+			} else if (response) {
+				result = response;
+			}
+			StackMob.onsuccess(model, method, params, result, success, options);
+ 
+		};
+		params['success'] = defaultSuccess;
+ 
+		var xhr = Ti.Network.createHTTPClient({
+			onload : function(e) {
+				params['success'](xhr);
+			},
+			onerror : function(e) {
+				params['error'](xhr)
+			},
+			timeout : 5e3
+		});
+ 
+		// if logging in...
+		if (StackMob.isOAuth2Mode() && (method === 'accessToken' || method === 'facebookAccessToken')) {
+			xhr.open(params.type, params.url + "?" + params.data);
+			params['beforeSend'](xhr, params);
+			return xhr.send();
+		} else {
+			xhr.open(params.type, params.url);
+			params['beforeSend'](xhr, params);
+			// if not 'GET' then post body here!!
+			return xhr.send(params.type !== 'GET' ? params.data : null);
+		}
+ 
+	}; 
+	StackMob.init(options);
+ 
+	return StackMob;
+};
+module.exports = TiStackMob;
